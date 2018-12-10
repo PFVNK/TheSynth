6bbe8b451ba1da941915b62e85e3fb426547b7fd
Patrick Heflin <patrickslx@yahoo.com>
2018-12-10 08:30:21 -0600

    added function to trigger synth notes

--- src/Components/Pads.js
+++ src/Components/Pads.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import '../App.sass';
+import Tone from 'tone'
 
 export class Pads extends Component {
+
     render() {
+        function triggerNote() {
+            const synth = new Tone.Synth()
+            synth.toMaster()
+
+            synth.triggerAttackRelease('c4', '8n')
+
+            let notes = ['a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3']
+        }
+
         return (
             <div className='pad-grid'>
-                <div className='pad'>1</div>
+                <div className='pad' onClick={triggerNote}>1</div>
                 <div className='pad'>2</div>
                 <div className='pad'>3</div>
                 <div className='pad'>4</div>
