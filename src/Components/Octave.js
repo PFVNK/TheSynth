5a25b85ed458d2db5f88c4468a650efaaa1879f6
Patrick Heflin <patrickslx@yahoo.com>
2019-01-02 21:00:44 -0600

    brought in octave variable & handleClickOctave() from react context

--- src/Components/Octave.js
+++ src/Components/Octave.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Consumer from './SynthContext';
+
 
 class Octave extends Component {
+
     render() {
+        const { handleClickOctave } = this.context
         return (
             <div>
-                <div className='octave-counter'>
-                    <div>Octave:</div>
-                    <button onClick={() => this.props.handleClick('minus')}>-</button>
-                    <p>{this.props.octave}</p>
-                    <button onClick={() => this.props.handleClick('plus')}>+</button>
-                </div>
+                <Consumer>
+                    {octave => {
+                        return (
+                            <div className='octave-counter'>
+                                <div className='control-label'>Octave:</div>
+                                <button onClick={() => handleClickOctave('minus')}>-</button>
+                                <p>{octave.octave}</p>
+                                <button onClick={() => handleClickOctave('plus')}>+</button>
+                            </div>
+                        )
+                    }}
+                </Consumer>
             </div>
         )
     }
 }
+
+Octave.contextType = Consumer
 
 export default Octave;
 
