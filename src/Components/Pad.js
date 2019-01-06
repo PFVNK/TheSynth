92d87fca5243bf18f16f64cbc5fef59c23afab55
Patrick Heflin <patrickslx@yahoo.com>
2019-01-06 09:27:50 -0600

    stripped all functionality from component

--- src/Components/Pad.js
+++ src/Components/Pad.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react';
 import '../App.sass';
-import PropTypes from 'prop-types'
 
 
-class Pad extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            clicked: false
-        };
-
-        this.clickedMouse = this.clickedMouse.bind(this);
-        this.unClickedMouse = this.unClickedMouse.bind(this);
-    }
-
-    clickedMouse(e) {
-        this.setState({ clicked: true });
-        this.props.onDown(this.props.note);
-    }
-    unClickedMouse(e) {
-        this.setState({ clicked: false });
-        this.props.onUp(this.props.note);
-    }
-
+class Pad extends Component {
     render() {
         return (
             <div
-                className='pad'
-                onMouseUp={this.unClickedMouse}
-                onMouseDown={this.clickedMouse}
+                className="pad"
+                tabIndex="0"
             />
         );
     }
 }
 
+
 export default Pad
 
-Pad.propTypes = {
-    onDown: PropTypes.func,
-    onUp: PropTypes.func,
-    note: PropTypes.string
-}
