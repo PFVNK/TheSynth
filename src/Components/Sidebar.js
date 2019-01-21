4af866c3b631ee5f811f4470dec8f191350d5d78
Patrick Heflin <patrickslx@yahoo.com>
2019-01-21 13:19:53 -0600

    added oscillator select options & added handler for osciallator select

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,8 +8,13 @@ class Sidebar extends Component {
         this.props.updateSynthType(synth)
     }
 
+    handleOscillatorChange = (e) => {
+        let oscillator = e.target.value
+        this.props.updateOscillatorType(oscillator)
+    }
+
     render() {
-        let { synthvalue } = this.props
+        let { synthvalue, oscvalue } = this.props
         return (
             <div className='sidebar-element'>
                 <div className='sidebar-synthtype'>
@@ -19,6 +24,15 @@ class Sidebar extends Component {
                         <option value="AMSynth">AMSynth</option>
                         <option value="FMSynth">FMSynth</option>
                         <option value="PluckSynth">PluckSynth</option>
+                    </select>
+                </div>
+                <div className='sidebar-oscillatortype'>
+                    <h4>OscillatorType</h4>
+                    <select value={oscvalue} onChange={this.handleOscillatorChange}>
+                        <option value="triangle">triangle</option>
+                        <option value="sawtooth">sawtooth</option>
+                        <option value="sine">sine</option>
+                        <option value="square">square</option>
                     </select>
                 </div>
             </div>
