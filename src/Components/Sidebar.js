75050dd744f62b2e0e7e2060ea29557b13f30fb8
Patrick Heflin <patrickslx@yahoo.com>
2019-01-23 06:27:37 -0600

    added functionality to control octave

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 
 class Sidebar extends Component {
@@ -16,28 +17,41 @@ class Sidebar extends Component {
     render() {
         let { synthvalue, oscvalue } = this.props
         return (
-            <div className='sidebar-element'>
-                <div className='sidebar-synthtype'>
-                    <h4>SynthType</h4>
-                    <select value={synthvalue} onChange={this.handleSynthChange}>
-                        <option value="Synth">Synth</option>
-                        <option value="AMSynth">AMSynth</option>
-                        <option value="FMSynth">FMSynth</option>
-                        <option value="PluckSynth">PluckSynth</option>
-                    </select>
+            <React.Fragment>
+                <div className='sidebar-element'>
+                    <div className='sidebar-synthtype'>
+                        <h4>SynthType</h4>
+                        <select value={synthvalue} onChange={this.handleSynthChange}>
+                            <option value="Synth">Synth</option>
+                            <option value="AMSynth">AMSynth</option>
+                            <option value="FMSynth">FMSynth</option>
+                            <option value="PluckSynth">PluckSynth</option>
+                        </select>
+                    </div>
+                    <div className='sidebar-oscillatortype'>
+                        <h4>OscillatorType</h4>
+                        <select value={oscvalue} onChange={this.handleOscillatorChange}>
+                            <option value="triangle">triangle</option>
+                            <option value="sawtooth">sawtooth</option>
+                            <option value="sine">sine</option>
+                            <option value="square">square</option>
+                        </select>
+                    </div>
+                    <div className='sidebar-octave-counter'>
+                        <h4>Octave</h4>
+                        <button onClick={() => this.props.handleClickOctave('minus')}>-</button>
+                        <p>{this.props.octave}</p>
+                        <button onClick={() => this.props.handleClickOctave('plus')}>+</button>
+                    </div>
                 </div>
-                <div className='sidebar-oscillatortype'>
-                    <h4>OscillatorType</h4>
-                    <select value={oscvalue} onChange={this.handleOscillatorChange}>
-                        <option value="triangle">triangle</option>
-                        <option value="sawtooth">sawtooth</option>
-                        <option value="sine">sine</option>
-                        <option value="square">square</option>
-                    </select>
-                </div>
-            </div>
+            </React.Fragment>
         )
     }
 }
 
 export default Sidebar
+
+Sidebar.propTypes = {
+    handleClickOctave: PropTypes.func,
+    octave: PropTypes.number
+}
